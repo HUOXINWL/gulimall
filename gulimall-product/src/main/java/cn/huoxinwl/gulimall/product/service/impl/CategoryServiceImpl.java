e8dbcd637b12c4cc8e4b62f57226712ab994dcc8
huoxin <HUOXINWL@163.com>
2023-02-17 13:55:32 +0800

    SpringCache - @CacheEvict、Spring-Cache的不足之处

--- gulimall-product/src/main/java/cn/huoxinwl/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/cn/huoxinwl/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -8,6 +8,7 @@ import org.redisson.api.RLock;
 import org.redisson.api.RReadWriteLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
@@ -84,8 +85,19 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     /**
      * 级联更新所有关联的数据
+     *
+     * @CacheEvict:失效模式
+     *      * @CachePut:双写模式，需要有返回值
+     *      * 1、同时进行多种缓存操作：@Caching
+     *      * 2、指定删除某个分区下的所有数据 @CacheEvict(value = "category",allEntries = true)
+     *      * 3、存储同一类型的数据，都可以指定为同一分区
      * @param category
      */
+    // @Caching(evict = {
+    //         @CacheEvict(value = "category",key = "'getLevel1Categorys'"),
+    //         @CacheEvict(value = "category",key = "'getCatalogJson'")
+    // })
+    @CacheEvict(value = "category",allEntries = true)//删除某个分区下的所有数据
     @Transactional
     @Override
     public void updateCascade(CategoryEntity category) {
@@ -107,7 +119,23 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
      *      指定缓存的数据的存活时间:配置文档中修改存活时间
      *      将数据保存为json格式
      *
-     * @return
+     * 4、Spring-Cache的不足之处：
+     *      *  1）、读模式
+     *      *      缓存穿透：查询一个null数据。解决方案：缓存空数据
+     *      *      缓存击穿：大量并发进来同时查询一个正好过期的数据。解决方案：加锁 ? 默认是无加锁的;使用sync = true来解决击穿问题
+     *      *      缓存雪崩：大量的key同时过期。解决：加随机时间。加上过期时间
+     *      *  2)、写模式：（缓存与数据库一致）
+     *      *      1）、读写加锁。
+     *      *      2）、引入Canal,感知到MySQL的更新去更新Redis
+     *      *      3）、读多写多，直接去数据库查询就行
+     *      *
+     *      *  总结：
+     *      *      常规数据（读多写少，即时性，一致性要求不高的数据，完全可以使用Spring-Cache）：写模式(只要缓存的数据有过期时间就足够了)
+     *      *      特殊数据：特殊设计
+     *      *
+     *      *  原理：
+     *      *      CacheManager(RedisCacheManager)->Cache(RedisCache)->Cache负责缓存的读写
+     *      * @return
      */
     @Cacheable(value = {"category"},key = "#root.method.name")
     @Override
@@ -116,6 +144,53 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         List<CategoryEntity> categoryEntities = this.baseMapper.selectList(
                 new QueryWrapper<CategoryEntity>().eq("parent_cid", 0));
         return categoryEntities;
+    }
+
+    @Cacheable(value = "category",key = "#root.methodName")
+    @Override
+    public Map<String, List<Catelog2Vo>>  getCatalogJson(){
+        System.out.println("查询了数据库");
+
+        /**
+         * 将数据库的多次查询变为一次
+         */
+        List<CategoryEntity> selectList = this.baseMapper.selectList(null);
+
+        //1、查出所有分类
+        //1、1）查出所有一级分类
+        List<CategoryEntity> level1Categorys = getParent_cid(selectList, 0L);
+
+        //封装数据
+        Map<String, List<Catelog2Vo>> parentCid = level1Categorys.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
+            //1、每一个的一级分类,查到这个一级分类的二级分类
+            List<CategoryEntity> categoryEntities = getParent_cid(selectList, v.getCatId());
+
+            //2、封装上面的结果
+            List<Catelog2Vo> catelog2Vos = null;
+            if (categoryEntities != null) {
+                catelog2Vos = categoryEntities.stream().map(l2 -> {
+                    Catelog2Vo catelog2Vo = new Catelog2Vo(v.getCatId().toString(), null, l2.getCatId().toString(), l2.getName().toString());
+
+                    //1、找当前二级分类的三级分类封装成vo
+                    List<CategoryEntity> level3Catelog = getParent_cid(selectList, l2.getCatId());
+
+                    if (level3Catelog != null) {
+                        List<Catelog2Vo.Category3Vo> category3Vos = level3Catelog.stream().map(l3 -> {
+                            //2、封装成指定格式
+                            Catelog2Vo.Category3Vo category3Vo = new Catelog2Vo.Category3Vo(l2.getCatId().toString(), l3.getCatId().toString(), l3.getName());
+
+                            return category3Vo;
+                        }).collect(Collectors.toList());
+                        catelog2Vo.setCatalog3List(category3Vos);
+                    }
+
+                    return catelog2Vo;
+                }).collect(Collectors.toList());
+            }
+
+            return catelog2Vos;
+        }));
+        return parentCid;
     }
 
     /**
@@ -129,8 +204,8 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
      *   lettuce、jedis 操作redis的底层客户端，Spring再次封装
      * @return
      */
-    @Override
-    public Map<String, List<Catelog2Vo>> getCatalogJson(){
+//    @Override
+    public Map<String, List<Catelog2Vo>> getCatalogJson2(){
         //给缓存中放json字符串，拿出的json字符串还要逆转为能用的对象类型【序列化与反序列化】
 
         //1. 加入缓存逻辑，缓存中村的数据是json字符串
