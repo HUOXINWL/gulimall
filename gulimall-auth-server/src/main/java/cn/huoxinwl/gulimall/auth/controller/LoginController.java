bfd5b9579c84e4ed3eb9c47ed28d9ff9bfce5616
huoxin <HUOXINWL@163.com>
2023-02-21 18:59:55 +0800

    商城业务 - 认证服务，手机短信验证码

--- gulimall-auth-server/src/main/java/cn/huoxinwl/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/cn/huoxinwl/gulimall/auth/controller/LoginController.java
@@ -1,10 +1,26 @@
 package cn.huoxinwl.gulimall.auth.controller;
 
+import cn.huoxinwl.common.constant.AuthServerConstant;
+import cn.huoxinwl.common.exception.BizCodeEnum;
+import cn.huoxinwl.common.utils.R;
+import cn.huoxinwl.gulimall.auth.feign.ThirdPartFeignService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private ThirdPartFeignService thirdPartFeignService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
 
     @GetMapping("/login.html")
     public String loginPage(){
@@ -14,5 +30,34 @@ public class LoginController {
     @GetMapping("/reg.html")
     public String regPage(){
         return "reg";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/sms/sendCode")
+    public R sendCode(@RequestParam("phone") String phone) {
+
+        //1、接口防刷
+        String redisCode = stringRedisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone);
+        if (!StringUtils.isEmpty(redisCode)) {
+            //活动存入redis的时间，用当前时间减去存入redis的时间，判断用户手机号是否在60s内发送验证码
+            long currentTime = Long.parseLong(redisCode.split("_")[1]);
+            if (System.currentTimeMillis() - currentTime < 60000) {
+                //60s内不能再发
+                return R.error(BizCodeEnum.SMS_CODE_EXCEPTION.getCode(),BizCodeEnum.SMS_CODE_EXCEPTION.getMsg());
+            }
+        }
+
+        //2、验证码的再次效验 redis.存key-phone,value-code
+        int code = (int) ((Math.random() * 9 + 1) * 100000);
+        String codeNum = String.valueOf(code);
+        String redisStorage = codeNum + "_" + System.currentTimeMillis();
+
+        //存入redis，防止同一个手机号在60秒内再次发送验证码
+        stringRedisTemplate.opsForValue().set(AuthServerConstant.SMS_CODE_CACHE_PREFIX+phone,
+                redisStorage,10, TimeUnit.MINUTES);
+
+        thirdPartFeignService.sendCode(phone, codeNum);
+
+        return R.ok();
     }
 }
